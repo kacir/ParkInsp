f17645ac7098186a9e05f0abde22638378118e99
Robert Kacir <kacir@wisc.edu>
2018-11-19 21:47:52 -0600

    Content added
    added maps and some substantive content to the pages such as form inputs, map content, and css style rules. Still needs a lot of work.

---
+++ js/navigate.js
@@ -0,0 +1,14 @@
+var mymap = L.map('map').setView([ 34.7517595, -92.329416], 8);
+
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+//add a marker icon and style it as due for inspection
+
+//add a marker icon and style it as needing inspection

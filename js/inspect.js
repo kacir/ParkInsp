195de11c00fa34dd92d33cc778879fa4efe6fc30
Robert Kacir <kacir@wisc.edu>
2018-11-20 12:49:04 -0600

    Nav popup and inspect aerial
    added a styled popup into the marker for the navigate page. Also addedd aerial background imagery for the inspect page.

---
+++ js/inspect.js
@@ -0,0 +1,10 @@
+var mymap = L.map('map');
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(mymap);
+
+
+
+
+mymap.setView([ 34.7517595, -92.329416], 8);
--- js/navigate.js
+++ js/navigate.js
@@ -12,3 +12,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 //add a marker icon and style it as due for inspection
 
 //add a marker icon and style it as needing inspection
+
+var marker = L.marker([ 34.7517595, -92.329416]).addTo(mymap);
+
+marker.bindPopup("<table> <tr><th><h3>Park Name</h3></th></tr> <tr><td><a href='ViewPast.html'>View Past Reports</a></td> </tr><tr><td><a href='inspect.html'>Inspect</a></td></tr> <tr><td>Drive Here</td></tr> </table>")

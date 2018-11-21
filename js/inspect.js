ffc46d012a0b18dc9bfd096d49872db18a7a1dfd
Robert Kacir <kacir@wisc.edu>
2018-11-21 13:31:08 -0600

    Past report elements and url parameters
    Created many elements inside the past reports page. Also added url parameters that move through the different pages.

--- js/inspect.js
+++ js/inspect.js
@@ -1,3 +1,14 @@
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
 var mymap = L.map('map');
 
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
@@ -8,3 +19,10 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
 
 
 mymap.setView([ 34.7517595, -92.329416], 8);
+
+console.log("the url parameter is: " + getQueryVariable("parkNum"));
+
+var viewpastlink = $("#viewpastlink");
+viewpastlink.attr("href" , viewpastlink.attr("href") + "?&parknum=" + getQueryVariable("parkNum"));
+var createreport = $("#createReportLink");
+createreport.attr("href" , createreport.attr("href") + "?&parknum=" + getQueryVariable("parkNum"));
--- js/navigate.js
+++ js/navigate.js
@@ -13,6 +13,10 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 
 //add a marker icon and style it as needing inspection
 
+parkName = "Burns Park Example!";
+parkNum = "OG456";
+
 var marker = L.marker([ 34.7517595, -92.329416]).addTo(mymap);
 
-marker.bindPopup("<table> <tr><th><h3>Park Name</h3></th></tr> <tr><td><a href='ViewPast.html'>View Past Reports</a></td> </tr><tr><td><a href='inspect.html'>Inspect</a></td></tr> <tr><td>Drive Here</td></tr> </table>")
+marker.bindPopup("<table> <tr><th><h3>" + parkName + "</h3></th></tr> <tr><td><a href='ViewPast.html'>View Past Reports</a></td> </tr><tr><td><a href='inspect.html?&parkNum=" + parkNum +"'>Inspect</a></td></tr> <tr><td>Drive Here</td></tr> </table>");
+
